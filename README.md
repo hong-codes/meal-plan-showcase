b19195cf9d01bef5f170e0b2168723f01ce41552
hongphucdang1 <hpd.dang@gmail.com>
2023-02-09 20:09:26 -0500

    Update index and main.js

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ Meal planning app.
 
 The goal of this meal planning app is to have a user choose a recipe in the database, then create a shopping list of items the user needs for that resipe. When the user is at the store, they can check off purchased items.
 
-Curerntly, a user is able to get one random recipe. 
+Currently, a user is able to get one random recipe. 
 
-Next steps:
+This app is using the https://www.themealdb.com/api.php API.
+
+Currently working on:
+* updating the hiddent titles with toggle
+* pulling in all the ingredients
 * pulling in all recipes that a user inputs. For example, if a user types in chicken, bring up all recipes with chicken in it.
 
 Future
--- main.js
+++ main.js
@@ -6,9 +6,12 @@ function getRandomRecipe(){
   .then(res => res.json())
   .then(data => {
     console.log(data.meals[0].strMeal)
-    document.querySelector('h3').innerText = data.meals[0].strMeal
+    document.querySelector('.recipeName').innerText = data.meals[0].strMeal
     document.querySelector('.recipeImg').src = data.meals[0].strMealThumb
+    document.querySelector('.recipeIng').innerText = data.meals[0].strIngredient1
     document.querySelector('.recipeInstr').innerText = data.meals[0].strInstructions
+    document.querySelector('#ingTitle').classList.toggle('hidden')
+    document.querySelector('#instrTitle').classList.toggle('hidden')
 
     // add list of recipe ingredients
 
@@ -22,22 +25,20 @@ function getRandomRecipe(){
 
 
 // This function is for grabbing the recipes user enters into the form. Create a form
-// document.querySelector('button').addEventListener('click', getRecipe)
+document.querySelector('.test-button').addEventListener('click', getRecipe)
 
-// function getRecipe(){
-//   let recipe = document.querySelector('input').value + " "
-//   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
+function getRecipe(){
+  let recipe = document.querySelector('input').value
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
 
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data.meals[0])
-//     document.querySelector('.recipe-card h2').innerText = data.meals[0].strMeal
-//     document.querySelector('img').src = data.meals[0].strMealThumb
-//   })
-//   .catch(err => {
-//     console.log(`error ${err}`)
-//   });
-
-
-// }
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.meals[0])
+    document.querySelector('.recipe-card h2').innerText = data.meals[0].strMeal
+    document.querySelector('.test-recipeImg').src = data.meals[0].strMealThumb
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  });
+}
 
