8b20881862b184131928aad54d9b615a816d4aaa
hongphucdang1 <hpd.dang@gmail.com>
2023-05-12 11:18:00 -0400

    Add code

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // Current Issues
-// 1. light box showing where the img is showing, where is this comming from?
+// 1. light box showing where the img is showing, where is this comming from? this is due to having img tag w/ so image (at first load), need to change to a div
 // 2. any items appended, don't get deleted when button is clicked. How to clear this?
 
 // CURRENTLY WORKING ON
-// 1. adding multiple ingredients to meals
+// 1. adding a recipe to the DOM in the random recipe page
+
+// FINISHED
+// 1. Adding multiple ingredients to meals
 
 // This function is for grabbing a random recipe when user clicks on "Random Recipe button"
-document.querySelector('button').addEventListener('click', getRandomRecipe)
+document.querySelector('#randomRecipeButton').addEventListener('click', getRandomRecipe)
+document.querySelector('addRecipe').addEventListener('click', addRecipe)
 
 function getRandomRecipe(){
   fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
@@ -18,6 +22,16 @@ function getRandomRecipe(){
     document.querySelector('.recipeInstr').innerText = data.meals[0].strInstructions
     document.querySelector('#instrTitle').innerText = "Instructions"
     document.querySelector('#ingTitle').innerText = "Ingredients"
+
+    // add a button - Add recipe to DOM
+    let parentElement = document.querySelector('.recipeName');
+    let addRecipe = document.createElement('button');
+    addRecipe.textContent = "Add to Favorites";
+    addRecipe.type = "button";
+    addRecipe.id = "favoriteButton"
+
+    // insertAfter(addRecipe, parentElement);
+    parentElement.appendChild(addRecipe);
 
     // clear previous searches
     let mealIngredients = document.getElementById("recipeIng");
@@ -51,20 +65,20 @@ function getRandomRecipe(){
 
 
 // This function is for grabbing the recipes user enters into the form. Create a form
-document.querySelector('.test-button').addEventListener('click', getRecipe)
+// document.querySelector('.test-button').addEventListener('click', getRecipe)
 
-function getRecipe(){
-  let recipe = document.querySelector('input').value
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
+// function getRecipe(){
+//   let recipe = document.querySelector('input').value
+//   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
 
-  .then(res => res.json())
-  .then(data => {
-    console.log(data.meals[0])
-    document.querySelector('.recipe-card h2').innerText = data.meals[0].strMeal
-    document.querySelector('.test-recipeImg').src = data.meals[0].strMealThumb
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  });
-}
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data.meals[0])
+//     document.querySelector('.recipe-card h2').innerText = data.meals[0].strMeal
+//     document.querySelector('.test-recipeImg').src = data.meals[0].strMealThumb
+//   })
+//   .catch(err => {
+//     console.log(`error ${err}`)
+//   });
+// }
 
